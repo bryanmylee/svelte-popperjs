3d45c7ed25427da0a4802dae53e20300da6c5165
Bryan Lee <bryanmylee@gmail.com>
2021-05-06 00:43:51 +0800

    Revert "fix: type declaration file not generated properly"
    This reverts commit 4999fe8a9d91be3789bdf72a7cd6a03d46206e1e.

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,22 @@
-import typescript from "@rollup/plugin-typescript";
-import pkg from "./package.json";
-import tsconfig from "./tsconfig.json";
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
+import tsconfig from './tsconfig.json';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const {
-  declaration,
-  outFile,
-  ...compilerOptions
-} = tsconfig.compilerOptions;
+const { declaration, declarationDir, ...compilerOptions } = tsconfig.compilerOptions;
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
-    { format: "cjs", file: pkg.main },
-    { format: "es", file: pkg.module },
+    { format: 'cjs', file: pkg.main },
+    { format: 'es', file: pkg.module },
   ],
-  external: "@popperjs/core",
+  external: '@popperjs/core',
   plugins: [
     typescript({
       tsconfig: false,
-      ...compilerOptions,
-    }),
-  ],
+      ...compilerOptions
+    })
+  ]
 };
+
