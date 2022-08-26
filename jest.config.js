09569d46088e950875c6ca4aff3ad93671606d63
Bryan Lee <bryanmylee@gmail.com>
2022-08-26 20:20:50 +1000

    nit: get rid of jest config

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	roots: ['.'],
-	testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
-	transform: {
-		'^.+\\.(ts)$': 'ts-jest',
-	},
-	coveragePathIgnorePatterns: ['./node_modules/', './tests/helpers.ts'],
-};
