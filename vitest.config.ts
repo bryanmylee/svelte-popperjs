0b1364b6b3e24b563b3773750c2184ed6f8950e1
Bryan Lee <bryanmylee@gmail.com>
2022-08-26 17:09:58 +1000

    build: move to vitest for test

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [svelte({ hot: !process.env.VITEST })],
+	test: {
+		environment: 'jsdom',
+		setupFiles: './vitest.setup.ts',
+		globals: true,
+	},
+});
